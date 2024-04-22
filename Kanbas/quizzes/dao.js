e94dd1f4382dc3aa99c801768fea5c16bef33598
stephanieP0826 <stephanie@firstprinciplespublishing.com>
2024-04-22 13:50:24 -0400

    fix

--- Kanbas/quizzes/dao.js
+++ Kanbas/quizzes/dao.js
@@ -5,8 +5,8 @@ export const createQuiz = (quiz) => {
     //delete quiz._id;
     return model.create(quiz);
 };
-export const findAllQuizzes = () => model.find();
-export const findQuizById = (id) => model.find({_id: id});
+export const findAllQuizzes = (courseId) => model.find({ course: courseId });
+export const findQuizById = (id) => model.findById(id);
 export const findQuizByName = (quizName) => model.findOne({quizName});
 export const updateQuiz = (id, quiz) =>
     model.updateOne({ _id: id }, { $set: quiz });
--- Kanbas/quizzes/routes.js
+++ Kanbas/quizzes/routes.js
@@ -2,10 +2,18 @@ import * as dao from "./dao.js"
 
 export default function QuizRoutes(app) {
   const findQuizById = async (req, res) => {
-    const { quizId } = req.params;
-    const quiz = await dao.findQuizById(quizId);
-    res.json(quiz);
-  };
+    try {
+        const { quizId } = req.params;
+        const quiz = await dao.findQuizById(quizId);
+        if (!quiz) {
+            return res.status(404).json({ message: 'Quiz not found' });
+        }
+        res.json(quiz);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
 
   const findQuizzesForCourse = async (req, res) => {
     const {courseId} = req.params;
@@ -14,11 +22,12 @@ export default function QuizRoutes(app) {
   };
 
   const createQuiz = async (req, res) => {
-    const {courseId} = req.params;
-    const newQuiz = await dao.createQuiz({...req.body, course: courseId});
+    const { courseId } = req.params;
+    const newQuiz = { ...req.body, course: courseId };
     const quiz = await dao.createQuiz(newQuiz);
     res.json(quiz);
-  };
+};
+
 
   const updateQuiz = async (req, res) => {
     const { quizId } = req.params;
@@ -35,6 +44,7 @@ export default function QuizRoutes(app) {
   app.get("/api/courses/:course/quizzes/:quizId", findQuizById);
   app.get("/api/courses/:course/quizzes", findQuizzesForCourse);
   app.post("/api/courses/:course/quizzes", createQuiz);
-  app.put("/api/quizzes/:quiz", updateQuiz);
-  app.delete("/api/quizzes/:quiz", deleteQuiz);
+  app.put("/api/quizzes/:quizId", updateQuiz);
+  app.delete("/api/quizzes/:quizId", deleteQuiz);
+  
 }
