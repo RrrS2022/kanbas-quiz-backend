84a28a08cbf5055ec14075b315926a144d488e29
Johnny <yeungho12@gmail.com>
2024-04-22 19:19:10 -0400

    fixed question

--- Kanbas/questions/dao.js
+++ Kanbas/questions/dao.js
@@ -2,13 +2,16 @@ import questionModel from "./model.js";
 export const createQuestion = (question) => {
     return questionModel.create(question);
 }
-export const findQuestionsForQuiz = (quizId) => 
+export const findQuestionsForQuiz = (quizId) =>
     questionModel.find({ quiz: quizId });
-export const updateQuestion = (questionId, question) => 
+
+export const updateQuestion = (questionId, question) =>
     questionModel.updateOne({ _id: questionId }, { $set: question });
-export const deleteQuestion = (questionId) => 
+
+export const deleteQuestion = (questionId) =>
     questionModel.deleteOne({ _id: questionId });
-export const findQuestionById = (questionId) => 
+
+export const findQuestionById = (questionId) =>
     questionModel.find({questionId});
 
 
--- Kanbas/quizzes/dao.js
+++ Kanbas/quizzes/dao.js
@@ -10,4 +10,4 @@ export const findQuizById = (quizId) => model.findById(quizId);
 export const findQuizByName = (quizName) => model.findOne({quizName});
 export const updateQuiz = (id, quiz) =>
     model.updateOne({ _id: id }, { $set: quiz });
-export const deleteQuiz = (id) => model.deleteOne({ _id: id });
+export const deleteQuiz = (id) => model.deleteOne({ _id: id }); 
